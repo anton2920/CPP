04327a8004830dd644c6795515436893cfc7311b
anton <anton2920@gmail.com>
2019-09-04 19:33:43 +0300

    DELL: add dev project

---
+++ lab/lab_dev/cmake-build-debug/CMakeFiles/lab_dev.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_dev.dir/main.cpp.o"
+  "lab_dev.pdb"
+  "lab_dev"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab_dev.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
